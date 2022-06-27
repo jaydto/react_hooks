c129328a29e7e88e5505dcff27ac0f5f07b165c8
jaydto <johnckaris@gmail.com>
2022-06-27 08:58:26 +0300

    introduced a json server

--- src/data/Table.js
+++ src/data/Table.js
@@ -1,47 +1,47 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 
 const Table = () => {
+  const [data,setData]=useState([]);
+  useEffect(()=>{
+    fetch("http://localhost:3000/data",{
+    headers:{
+    "Content-Type":"application/json",
+    "Accept":"application/json"}})
+    .then((response)=>response.json())
+    .then((data)=>{
+      setData(data);
+    },[])
+  })
   return (
     <div >
-           <h1 class="display-3 bg-dark text-center">Books in store</h1>
-      <table class="table table-striped">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Author</th>
-      <th scope="col">Book</th>
-      <th scope="col">price</th>
-      <th scope="col">Action</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Author Leah</td>
-      <td>Apple</td>
-      <td>25$</td>
-      <td><button type="submit" className="btn btn-success">book</button></td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jack</td>
-      <td>Arizona</td>
-      <td> 10$</td>
-      <td><button type="submit" className="btn btn-success">book</button></td>
+           <><h1 class="display-3 bg-dark text-center">Books in store</h1><table class="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Author</th>
+              <th scope="col">Book</th>
+              <th scope="col">price</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+      {data.map((item)=>( 
 
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>The dots</td>
-      <td>Exclusive</td>
-      <td><button type="submit" className="btn btn-success">book</button></td>
+            <tr>
+              <th scope="row">{item.id}</th>
+              <td>{item.name}</td>
+              <td>{item.book}</td>
+              <td>{item.price}</td>
+              <td><button type="submit" className="btn btn-success">book</button></td>
+            </tr>
+      
+      ))}
+          </tbody>
+        </table></>
 
-    </tr>
-  </tbody>
-</table>
     </div>
   )
 }
+
 
 export default Table
